aa937906b9b2f2e73e2c25d41f72b9e777588b51
Quim Nieto <quimnieto@MacBook-Pro-de-Tecnico.local>
2023-05-18 09:28:09 +0200

    Event bus implemented

--- src/Backoffice/Clients/Application/Create/ClientCreator.php
+++ src/Backoffice/Clients/Application/Create/ClientCreator.php
@@ -6,11 +6,14 @@ namespace Qm\Backoffice\Clients\Application\Create;
 
 use Qm\Backoffice\Clients\Domain\Aggregate\Client;
 use Qm\Backoffice\Clients\Domain\Repository\ClientRepository;
+use Qm\Shared\Domain\Bus\Event\EventBus;
 
 readonly class ClientCreator
 {
-    public function __construct(private ClientRepository $repository)
-    {
+    public function __construct(
+        private ClientRepository $repository,
+        private EventBus $bus,
+    ) {
     }
 
     public function create(CreateClientCommand $command): void
@@ -22,5 +25,6 @@ readonly class ClientCreator
         );
 
         $this->repository->save($client);
+        $this->bus->publish(...$client->pullDomainEvents());
     }
 }
--- src/Shared/Domain/Aggregate/AggregateRoot.php
+++ src/Shared/Domain/Aggregate/AggregateRoot.php
@@ -4,6 +4,22 @@ declare(strict_types=1);
 
 namespace Qm\Shared\Domain\Aggregate;
 
+use Qm\Shared\Domain\Bus\Event\DomainEvent;
+
 abstract class AggregateRoot
 {
+    private array $domainEvents = [];
+
+    public function pullDomainEvents(): array
+    {
+        $domainEvents = $this->domainEvents;
+        $this->domainEvents = [];
+
+        return $domainEvents;
+    }
+
+    public function record(DomainEvent $domainEvent): void
+    {
+        $this->domainEvents[] = $domainEvent;
+    }
 }
